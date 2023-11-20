2f535f431154dde45436acfa5cd91b58efac2334
wooseob <dntjq5130@naver.com>
2023-11-20 11:31:07 +0900

    Readme 수정

--- models/index.js
+++ models/index.js
@@ -1,48 +1,40 @@
-"use strict";
+// models/index.js
 
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const process = require("process");
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
+const modelDefiner = require('./modelDefiner'); // Correct path to modelDefiner
 
-let sequelize;
+let sequelizeInstance; // 여기에 Sequelize 인스턴스를 생성하는 코드를 작성
+
 if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+    sequelizeInstance = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-    sequelize = new Sequelize(
-        config.database,
-        config.username,
-        config.password,
-        config
-    );
+    sequelizeInstance = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs.readdirSync(__dirname)
-    .filter(
-        (file) =>
-        file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    )
-    .forEach((file) => {
-        console.log("model file:", file);
-        // eslint-disable-next-line global-require,import/no-dynamic-require
-        const model = require(path.join(__dirname, file))(
-            sequelize,
-            Sequelize.DataTypes
-        );
-        db[model.name] = model; // db.User(키) = User(모델 객체)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = modelDefiner(sequelizeInstance, Sequelize); // Using modelDefiner correctly
+        db[model.name] = model;
     });
 
-Object.keys(db).forEach((modelName) => {
+Object.keys(db).forEach(modelName => {
     if (db[modelName].associate) {
         db[modelName].associate(db);
     }
 });
 
-db.sequelize = sequelize;
+db.sequelize = sequelizeInstance;
 db.Sequelize = Sequelize;
 
-module.exports = db;
+module.exports = { db, modelDefiner }; // db와 modelDefiner를 함께 내보냄
